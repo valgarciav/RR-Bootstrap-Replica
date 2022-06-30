20503d4c454ebdc81236480fb68e34b976af9540
valgarciav <vkgarcia@gmail.com>
2022-06-30 19:42:09 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import TopBar from "./Component/TopBar";
+import Background from "./Component/Background";
+import StoreListings from "./Component/StoreListing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <Background />
+      <StoreListings />
     </div>
   );
 }
